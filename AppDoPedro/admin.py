37e56ca6ad06947db075533ce3752958d030fb3c
barella-pedro <146791786+barella-pedro@users.noreply.github.com>
2023-11-20 20:40:49 +0000

    Teste

--- AppDoPedro/admin.py
+++ AppDoPedro/admin.py
@@ -8,8 +8,8 @@ from django.contrib import admin
 # admin.site.register(Medias_Jogadores)
 # admin.site.register(Lista_Usuarios)
 
-from .models import Jogadores, Usuario, User
+from .models import Jogadores, Usuario, Userlist
 
 admin.site.register(Jogadores)
 admin.site.register(Usuario)
-admin.site.register(User)
+admin.site.register(Userlist)
--- django_project/urls.py
+++ django_project/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from AppDoPedro import views 
 urlpatterns = [
   path('',views.home, name="home"),
+   path('users',views.create_user),
+  path('users/login/',views.login_user, name="login"),
+  path('users/logout/',views.logout_user, name="logout"),
   path('listas_usuarios', views.crie_lista),
   path('listas_usuarios/update/<id>', views.atualize_lista),
   path('listas_usuarios/delete/<id>', views.delete_lista),
-  path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls)
 ]
